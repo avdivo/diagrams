616b2f5364b2599c171dfbc1ce9d3f2de346498d
avdivo <avdivo@mail.ru>
2023-02-21 23:44:42 +0300

    example

--- diagram.py
+++ diagram.py
@@ -1,7 +1,16 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Diagram, Edge, Node
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+with Diagram('My Diagram', direction='TB'):
+    n1 = Node('n1', style='filled', fillcolor='#40e0d0')
+    n2 = Node('n2')
+    n3 = Node('n3')
+    n4 = Node('n4')
+    n5 = Node('n5')
+    n6 = Node('n6')
+
+    n1 >> n2
+    n1 >> n3
+    n1 >> n4
+    n1 >> n5
+    n3 - n4
+    n5 >> Edge(label='This is a label', color='red') >> n6
